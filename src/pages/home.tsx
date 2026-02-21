addebefbb36734fe5438e14741321e4c914f9a63
Ellias Ribeiro <56983572+sailleribeiro@users.noreply.github.com>
2026-02-21 11:44:43 -0300

    feat: integrate Tabs component into Home page for improved navigation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,34 @@
 import { UserProfile } from "@/components/user-profile";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { BookMarked, Star } from "lucide-react";
+
 export function Home() {
   return (
-    <div className="flex flex-col gap-4 items-center py-4">
+    <div className="flex flex-col gap-4 items-center p-4">
       <UserProfile />
+
+      <Tabs defaultValue="repositories">
+        <TabsList>
+          <TabsTrigger value="repositories">
+            <BookMarked size={24} />
+            Repositories
+            <div className="flex text-sm justify-center text-gray-400 bg-gray-50 w-10 border border-gray-300 rounded-full px-6 py-1 ">
+              123
+            </div>
+          </TabsTrigger>
+          <TabsTrigger value="starred">
+            <Star size={24} />
+            Starred
+            <div className="flex text-sm justify-center text-gray-400 bg-gray-50 w-10 border border-gray-300 rounded-full px-6 py-1 ">
+              123
+            </div>
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="repositories">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="starred">Change your password here.</TabsContent>
+      </Tabs>
     </div>
   );
 }
