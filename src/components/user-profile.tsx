29695af976df2507b0ef1ee0cf91a3808b5359c8
Ellias Ribeiro <56983572+sailleribeiro@users.noreply.github.com>
2026-02-21 14:29:17 -0300

    feat: enhance UserProfile component with additional user information and improved layout

--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -1,11 +1,11 @@
 import { useGithubUser } from "@/hooks/use-github-user";
-import { ChevronDown } from "lucide-react";
+import { Building2, ChevronUp, Link, MapPin } from "lucide-react";
 
 export const UserProfile = () => {
   const user = useGithubUser("sailleribeiro");
 
   return (
-    <div className="flex flex-col gap-3 items-center py-4">
+    <div className="flex flex-col gap-3 items-center py-4 w-full">
       <img
         src={user.data?.avatar_url}
         alt="avatar"
@@ -17,8 +17,22 @@ export const UserProfile = () => {
 
       <div className="text-blue-400 flex flex-col items-center gap-1 cursor-pointer">
         <p>Informações Adicionais</p>
-        <ChevronDown size={24} />
+        <ChevronUp size={24} />
       </div>
+
+      <section className="bg-gray-50 p-3 rounded-lg flex flex-col gap-2 w-full text-blue-400">
+        <span className="flex gap-3 items-center">
+          <Building2 size={16} /> {user.data?.company}
+        </span>
+        <span className="flex gap-3 items-center">
+          <MapPin size={16} /> {user.data?.location}
+        </span>
+        <span className="flex gap-3 items-center">
+          <Link size={16} />
+          {user.data?.blog}
+        </span>
+        <span className="flex gap-3 items-center">{user.data?.email}</span>
+      </section>
     </div>
   );
 };
