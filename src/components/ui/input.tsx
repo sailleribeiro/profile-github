55327434f6e0018e431eab88ebf5643dc3a7155c
Ellias Ribeiro <56983572+sailleribeiro@users.noreply.github.com>
2026-02-21 18:45:40 -0300

    fix: refactor Input component's className for improved readability and maintainability

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,7 +13,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           ref={ref}
           type={type}
           className={cn(
-            "flex h-12 w-full rounded-md bg-muted md:bg-transparent md:rounded-none md:border-b md:border-b-muted-foreground/50 px-4 py-2 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring md:focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "flex h-12 w-full rounded-md bg-muted px-4 py-2 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "md:bg-transparent md:rounded-none md:border-0 md:border-b md:border-b-muted-foreground/50",
+            "md:focus-visible:ring-0 md:focus-visible:border-b-muted-foreground",
             icon && "pr-12",
             className,
           )}
