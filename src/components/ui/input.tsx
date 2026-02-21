f787996e52aee55e6a24a025913c9179a0b3aa57
Ellias Ribeiro <56983572+sailleribeiro@users.noreply.github.com>
2026-02-21 18:46:39 -0300

    fix: adjust padding and positioning for icon in Input component for better layout

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -16,13 +16,13 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             "flex h-12 w-full rounded-md bg-muted px-4 py-2 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
             "md:bg-transparent md:rounded-none md:border-0 md:border-b md:border-b-muted-foreground/50",
             "md:focus-visible:ring-0 md:focus-visible:border-b-muted-foreground",
-            icon && "pr-12",
+            icon && "pr-12 md:pr-4 md:pl-8",
             className,
           )}
           {...props}
         />
         {icon && (
-          <div className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 text-muted-foreground [&_svg]:size-5">
+          <div className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 text-muted-foreground md:right-auto md:left-0 [&_svg]:size-5">
             {icon}
           </div>
         )}
