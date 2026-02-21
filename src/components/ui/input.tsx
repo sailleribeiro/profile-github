138f22f779969856d56a8bf5e69245814f4fc2d9
Ellias Ribeiro <56983572+sailleribeiro@users.noreply.github.com>
2026-02-21 18:44:54 -0300

    fix: correct formatting in Input component's className for consistency

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,7 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           ref={ref}
           type={type}
           className={cn(
-            "flex h-12 w-full rounded-md bg-muted md:bg-transparent md:rounded-none md:border-b md:border-b-muted-foreground/50 px-4 py-2 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "flex h-12 w-full rounded-md bg-muted md:bg-transparent md:rounded-none md:border-b md:border-b-muted-foreground/50 px-4 py-2 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring md:focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
             icon && "pr-12",
             className,
           )}
