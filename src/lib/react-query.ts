78b243d1d970751cd8e8e8e848488b60c69c144b
Ellias Ribeiro <56983572+sailleribeiro@users.noreply.github.com>
2026-02-21 21:09:24 -0300

    feat: configure QueryClient with default options for queries and mutations

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,3 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
