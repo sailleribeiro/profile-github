047834d97220013247c9815baf3d1903390ade57
Ellias Ribeiro <56983572+sailleribeiro@users.noreply.github.com>
2026-02-22 22:39:02 -0300

    fix: enhance starred repositories functionality with conditional fetching

--- src/hooks/use-github-starred.ts
+++ src/hooks/use-github-starred.ts
@@ -2,13 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { githubService } from "@/service/github-service";
 import { useGithubStore } from "@/store/github-store";
 
-export function useGithubStarred(username: string) {
+export function useGithubStarred(username: string, isEnabled: boolean = true) {
   const setStarredRepos = useGithubStore((s) => s.setStarredRepos);
 
   return useQuery({
     queryKey: ["github-starred-repos", username],
     queryFn: () => githubService.getStarredRepos(username),
-    enabled: !!username,
+    enabled: !!username && isEnabled,
     onSuccess: (data) => setStarredRepos(data),
   });
 }
